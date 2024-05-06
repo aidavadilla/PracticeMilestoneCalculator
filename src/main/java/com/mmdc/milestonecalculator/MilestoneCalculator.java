3e34e7df1eb40a225c9789458c97485570012271
aidavadilla <lr.aidavadilla@mmdc.mcl.edu.ph>
2024-05-06 19:25:04 +0900

    Initial commit

---
+++ src/main/java/com/mmdc/milestonecalculator/MilestoneCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mmdc.milestonecalculator;
+
+/**
+ *
+ * @author Aly
+ */
+public class MilestoneCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
